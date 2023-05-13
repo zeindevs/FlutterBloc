a2be9271bca7b8bfde687fe417732491879a367c
zeindevs <aqiladevaniaa@gmail.com>
2023-05-13 18:38:28 +0700

    add: 14-extension-method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_base/bloc/user.dart';
+import 'package:flutter_base/bloc/counter.dart';
 import 'package:flutter_base/pages/home.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => UserBloc(),
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
         child: const HomePage(),
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_base/bloc/user.dart';
+import 'package:flutter_base/bloc/counter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,46 +7,49 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UserBloc user = context.read<UserBloc>();
+    // Extension Method
+    // 1. context.read()
+    // 2. context.watch()
+    // 3. context.select()
+    CounterBloc counter = context.read<CounterBloc>(); // read one times
+    // CounterBloc counter = context.watch<CounterBloc>(); // read multiple times
+    // int value = context.select<CounterBloc, int>((value) => value.state);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(20),
-        children: [
-          BlocSelector<UserBloc, Map<String, dynamic>, String>(
-            selector: (state) => state["name"],
-            builder: (context, state) {
-              return Text("Name: $state");
-            },
-          ),
-          BlocSelector<UserBloc, Map<String, dynamic>, int>(
-            selector: (state) => state["age"],
-            builder: (context, state) {
-              return Text("Age: $state");
-            },
-          ),
-          const SizedBox(height: 20),
-          TextField(
-            onChanged: (value) => user.changeName(value),
-            decoration: const InputDecoration(border: OutlineInputBorder()),
-          ),
-          const SizedBox(height: 20),
-          Row(
+        appBar: AppBar(
+          title: const Text("Home"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              IconButton(
-                onPressed: () => user.changeAge(user.state["age"] - 1),
-                icon: const Icon(Icons.remove),
-              ),
-              IconButton(
-                onPressed: () => user.changeAge(user.state["age"] + 1),
-                icon: const Icon(Icons.add),
-              ),
+              // Builder(builder: (context) {
+              //   CounterBloc counterBloc = context.watch<CounterBloc>();
+              //   return Text(
+              //     "${counterBloc.state}",
+              //     style: const TextStyle(fontSize: 100),
+              //   );
+              // }),
+              Builder(builder: (context) {
+                int value =
+                    context.select<CounterBloc, int>((value) => value.state);
+                return Text(
+                  "$value",
+                  style: const TextStyle(fontSize: 100),
+                );
+              }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                      onPressed: () => counter.decrement(),
+                      icon: const Icon(Icons.remove)),
+                  IconButton(
+                      onPressed: () => counter.increment(),
+                      icon: const Icon(Icons.add)),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
